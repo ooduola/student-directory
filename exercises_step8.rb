0ac913e8732214aaa0605debbe0d3b36bb986e91
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-05 21:32:18 +0100

    Step 8. Exercise 2 completed

--- exercises_step8.rb
+++ exercises_step8.rb
@@ -1,5 +1,4 @@
 # let's put all students into an array
-
 def print_header
   puts  "The students of Villains Academy"
   puts "-------------" 
@@ -20,25 +19,26 @@ def input_students
     # get another name from the user
     name = gets.chomp.capitalize
   end
-  # return the array of students
   students
 end 
 
-def print(students)
-  students.each_with_index do |student, i|
-    puts " #{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end 
+def filtered_students
+  puts  "which students whose name begins with a specific letter would you like to see?"
+  letter = gets.chomp.upcase
 end
-
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+# collect the students names with whose name begins with first letter.
+def print(students, first_letter)
+  filtered = students.map do |student| 
+    if student[:name][0] == first_letter
+      "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+  filtered.delete(nil)
+  puts filtered 
+  puts "Overall, we have #{filtered.count} students with name starting #{first_letter}"
 end
-
-
-
-#nothing happens until we call the methods
 
 students = input_students
 print_header
-print(students)
-print_footer(students)
+first_letter = filtered_students
+print(students, first_letter)
