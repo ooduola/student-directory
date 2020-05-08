0c19677b472b477592919c45db4b2219c1675026
Olatunji Oduola <ola.oduola@gmail.com>
2020-05-08 10:03:04 +0100

    Rectified print header method typos

--- typos.rb
+++ typos.rb
@@ -17,9 +17,9 @@ def input_students
   students
  end
 
-def print-header
-  put "The students of my cohort at Makers Academy'
-  puts ``-------------``
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
 def print(students)
